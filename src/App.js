34852a3b80f03b53c85fdd27dd8ebc07537fa0c4
jkl83 <jkl834315106@qq.com>
2023-08-30 17:25:00 +0800

    'form'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Layout from './pages/Layout';
-import Login from './pages/Login';
 import { Button } from 'antd';
+import Login from '@/pages/Login'
 
 function App() {
   return (
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,38 @@
+import logo from '@/assets/logo.png'
+import './index.scss'
+import { Card, Form, Input, Button, Checkbox} from 'antd'
+
 function Login(){
   return (
-    <div>
-      login
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        <Form
+          initialValues={{
+            remember: true,
+          }}
+        >
+          <Form.Item>
+            <Input size="large" placeholder="请输入手机号"/>
+          </Form.Item>
+          <Form.Item>
+            <Input size="large" placeholder="请输入验证码"/>
+          </Form.Item>
+          <Form.Item
+            name="remember"
+            valuePropName='checked'
+          >
+            <Checkbox className="login-checkbox-label">
+              我已阅读并同意
+            </Checkbox>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
     </div>
   )
 }
