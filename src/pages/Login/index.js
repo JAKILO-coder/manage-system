fa3ea7d6c4e8e614d9b68825640743af455a2c18
jkl83 <jkl834315106@qq.com>
2023-08-31 11:33:57 +0800

    'login'

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,19 +3,55 @@ import './index.scss'
 import { Card, Form, Input, Button, Checkbox} from 'antd'
 
 function Login(){
+  function onFinish (values){
+    console.log(values)
+    
+  }
+  function onFinishFailed (errorInfo){
+    console.log('Failed:', errorInfo);
+  };
+  
   return (
     <div className="login">
       <Card className="login-container">
         <img className="login-logo" src={logo} alt="" />
-        <Form
+        <Form validateTrigger={['onBlur', 'onChange']}
           initialValues={{
             remember: true,
           }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
         >
-          <Form.Item>
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'please input phone number'
+              },
+              {
+                pattern:/^1[3-9]\d{9}$/,
+                message:'请输入正确的手机号',
+                validateTrigger: 'onBlur'
+              }
+            ]}
+          >
             <Input size="large" placeholder="请输入手机号"/>
           </Form.Item>
-          <Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'please input code'
+              },
+              {
+                len: 6,
+                message: '请输入6位密码',
+                validateTrigger: 'onBlur'
+              }
+            ]}
+          >
             <Input size="large" placeholder="请输入验证码"/>
           </Form.Item>
           <Form.Item
